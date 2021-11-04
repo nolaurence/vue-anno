3c250fe921b49c84a87e067bc026e9cffacabee8
nolaurence <nolaurence@outlook.com>
2021-11-04 21:31:08 +0800

    add login page

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import axios from "axios"
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
+  render: h => h(App),
   template: '<App/>'
 })
